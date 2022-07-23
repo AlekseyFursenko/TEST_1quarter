f938630617cec5d86c63f639b61d8cb0b1282f3c
Aleksey Fursenko <phursenko@hotmail.com>
2022-07-23 22:00:50 +0300

    draft code added

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void PrintArray(string[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+string[] mainArray = {"1234", "asd", "qwer", "1264", "asd"};
+
+PrintArray(mainArray);
+
+int symbLenghtSize = 3;
+int size = mainArray.Length;
+int count = 0; // Count of elements in the mainArray with lenght less or iqual symbLenghtSize
+string[] newArray = new string[size];
+
+for(int i = 0; i < size; i++)
+{
+    if(mainArray[i].Length <= symbLenghtSize)
+    {
+        newArray[count] = mainArray[i];
+        count++;
+    }
+}
+
+PrintArray(newArray);
