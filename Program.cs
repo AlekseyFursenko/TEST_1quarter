fe54a1bc64046ab95937cfe86fedc4f7328471d3
Aleksey Fursenko <phursenko@hotmail.com>
2022-07-24 19:16:53 +0300

    Input.txt added as string array

--- Program.cs
+++ Program.cs
@@ -7,14 +7,47 @@
     Console.WriteLine();
 }
 
-string[] mainArray = {"1234", "asd", "qwer", "1264", "^)", ":-)"};
+string[] ArrayInput()
+{
+    Console.WriteLine("Please choose the method of the main string array creation" + "\n" +
+                    "1 - manualy" + "\n" +
+                    "2 - from Input.txt file");
+    int method = Convert.ToInt32(Console.ReadLine());
+    
+    if(method == 1)
+    {
+        Console.WriteLine("Please input the size of your string array");
+        int size = Convert.ToInt32(Console.ReadLine());
+        string [] array = new string[size];
+
+        for(int i = 0; i < size; i++)
+        {
+            Console.Write("Please input " + (i + 1) + "element of the string array: ");
+            array[i] = Console.ReadLine();
+        }
+
+        return array;
+    }
+    if(method == 2)
+    {
+        Console.WriteLine("Please input the size of your string array")
+    }
+
+    return array;
+}
+
+// Main array create
+Console.Clear();
+
+
+
+string[] mainArray = ArrayInput();
 
 Console.WriteLine("Main string array:");
 PrintArray(mainArray);
 
 int symbLenghtSize = 3;
 int size = mainArray.Length;
-int count = 0; // Count of elements in the mainArray with lenght less or iqual symbLenghtSize
 
 File.WriteAllText("Temp.txt",String.Empty); // Clear the txt file for new temporary records
 
@@ -23,7 +56,6 @@ for(int i = 0; i < size; i++)
     if(mainArray[i].Length <= symbLenghtSize)
     {
         File.AppendAllText("Temp.txt", mainArray[i] + Environment.NewLine);
-        count++;
     }
 }
 
